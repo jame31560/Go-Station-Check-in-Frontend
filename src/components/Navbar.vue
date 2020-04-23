46247fbe99163f70a45925087b51e723dbbfed8b
JaoJao <jame31560@gmail.com>
2020-04-23 16:36:37 +0800

    init struct

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+    <a class="navbar-brand" href="#">GoStation Recorder</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" tag="a" class="nav-link">
+            首頁
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/login" tag="a" class="nav-link">
+            登入
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/register" tag="a" class="nav-link">
+            註冊
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/history" tag="a" class="nav-link">
+            踩點紀錄
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/account" tag="a" class="nav-link">
+            帳號資訊
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a href="javascript:;" class="nav-link" v-on:click="logout">
+            登出
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      console.log("logout");
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
+import VueAnalytics from "vue-analytics";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: "UA-164319527-1",
+  router,
+  autoTracking: {
+    pageviewOnLoad: false,
+  },
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
