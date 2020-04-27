d92c68aaec3742d66e784af5d2cc59c0a5cebb3a
JaoJao <jame31560@gmail.com>
2020-04-27 14:01:45 +0800

    update History

--- src/views/History.vue
+++ src/views/History.vue
@@ -10,19 +10,21 @@
         </tr>
       </thead>
       <tbody>
-        <tr :key="index" v-for="(h, index) in historys">
+        <tr
+          :key="index"
+          v-for="(h, index) in historys"
+        >
           <th scope="row">{{ index + 1 }}</th>
-          <td>{{ h.gostation_id }}</td>
+          <td>{{ gostations[h.gostation_id] }}</td>
           <td>
             {{ h.datetime | moment("YYYY年MM月DD日 HH:mm:ss") }}
           </td>
           <td>
             <a
               href="javascript:;"
-              v-on:click="delete h.id"
+              v-on:click="delete_record(h, index)"
               class="btn btn-danger btn-sm"
-              >刪除紀錄</a
-            >
+            >刪除紀錄</a>
           </td>
         </tr>
       </tbody>
@@ -31,53 +33,114 @@
 </template>
 
 <script>
-import moment from "moment";
-export default {
-  data() {
-    return {
-      historys: [],
-    };
-  },
-  computed: {
-    isLogin() {
-      return this.$store.state.isLogin;
-    },
-  },
-  methods: {
-    addEight(value) {
-      let date = new Date(value);
-      date.setTime(date.getTime() + 8 * 60 * 60 * 1000);
-      return date;
-    },
-  },
-  mounted() {
-    if (this.isLogin) {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${this.$store.state.token}`,
-        },
+  import moment from "moment";
+  export default {
+    data() {
+      return {
+        historys: [],
+        gostations: {}
       };
-      this.$http.defaults.headers.common = {
-        Authorization: `Bearer ${this.$store.state.token}`,
-      };
-      this.$http.get("http://192.168.1.102:5000/check_auth").then(
-        (res) => {
-          this.$http.get("http://192.168.1.102:5000/history").then((res) => {
-            this.historys = res.data.data;
-            console.log(this.historys);
-          });
-        },
-        (err) => {
-          this.$store.commit("SET_AUTH", {
-            token: "",
-            isLogin: false,
-          });
+    },
+    computed: {
+      isLogin() {
+        return this.$store.state.isLogin;
+      }
+    },
+    watch: {
+      isLogin(val) {
+        if (val == false) {
           this.$router.replace("/login");
         }
-      );
+      }
+    },
+    methods: {
+      delete_record(history, idx) {
+        let text = "你確定要刪除以下記錄嗎?\n刪除後不得復原。\n\n";
+        text += `地點：${this.gostations[history.gostation_id]}\n`;
+        text += `時間：${this.$moment(history.datetime).format(
+          "YYYY年MM月DD日 HH:mm:ss"
+        )}\n`;
+        if (confirm(text)) {
+          const config = {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.token}`
+            }
+          };
+          this.$http.defaults.headers.common = {
+            Authorization: `Bearer ${this.$store.state.token}`
+          };
+          this.$http.get("http://192.168.1.102:5000/check_auth").then(
+            res => {
+              this.$http
+                .delete("http://192.168.1.102:5000/history/" + history.id)
+                .then(
+                  res => {
+                    this.historys.splice(idx, 1);
+                    alert("刪除成功");
+                  },
+                  err => {
+                    this.$store.commit("SET_AUTH", {
+                      token: "",
+                      isLogin: false
+                    });
+                    this.$router.replace("/login");
+                  }
+                );
+            },
+            err => {
+              this.$store.commit("SET_AUTH", {
+                token: "",
+                isLogin: false
+              });
+              this.$router.replace("/login");
+            }
+          );
+        }
+      }
+    },
+    mounted() {
+      if (this.isLogin) {
+        const config = {
+          headers: {
+            Authorization: `Bearer ${this.$store.state.token}`
+          }
+        };
+        this.$http.defaults.headers.common = {
+          Authorization: `Bearer ${this.$store.state.token}`
+        };
+        this.$http.get("http://192.168.1.102:5000/check_auth").then(
+          res => {
+            this.$http.get("http://192.168.1.102:5000/history").then(res => {
+              this.historys = res.data.data;
+              console.log(this.historys);
+              let result = {};
+              this.$http.defaults.headers.common = {};
+              this.$http
+                .get("https://webapi.gogoro.com/api/vm/list")
+                .then(res => {
+                  let gostations = res.data;
+                  gostations.forEach((gostation, idx) => {
+                    result[gostation["Id"]] = JSON.parse(gostation["LocName"])[
+                      "List"
+                    ][1]["Value"];
+                  });
+                  this.gostations = result;
+                });
+            });
+          },
+          err => {
+            this.$store.commit("SET_AUTH", {
+              token: "",
+              isLogin: false
+            });
+            this.$router.replace("/login");
+          }
+        );
+      } else {
+        this.$router.replace("/login");
+      }
     }
-  },
-};
+  };
 </script>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
-    <GmapMap id="map" :center="center" :zoom="13" ref="gmap">
+    <GmapMap
+      id="map"
+      :center="center"
+      :zoom="13"
+      ref="gmap"
+    >
       <GmapMarker
         :key="index"
         v-for="(m, index) in markers"
@@ -32,11 +37,14 @@
           <p>
             {{ infoContent.state == 1 ? "正常營運中" : "建置/維護中" }}
           </p>
-          <p v-if="isLogin" class="text-center">
+          <p
+            v-if="isLogin && infoContent.state == 1"
+            class="text-center"
+          >
             <a
               href="javascript:;"
               class="btn btn-success"
-              v-on:click="checkin(infoContent.id)"
+              v-on:click="checkin(infoContent, currentMidx)"
             >
               簽到
             </a>
@@ -62,158 +70,163 @@
 </template>
 
 <script>
-export default {
-  name: "home",
-  data() {
-    return {
-      center: {
-        lat: null,
-        lng: null,
-      },
-      markers: [],
-      infoContent: null,
-      infoWindowPos: {
-        lat: 0,
-        lng: 0,
-      },
-      infoWinOpen: false,
-      currentMidx: null,
-      //optional: offset infowindow so it visually sits nicely on top of our marker
-      infoOptions: {
-        pixelOffset: {
-          width: 0,
-          height: -35,
+  export default {
+    name: "home",
+    data() {
+      return {
+        center: {
+          lat: null,
+          lng: null
         },
-      },
-      GoStations: [],
-      // marker_config: [],
-      history: [],
-      check_list: [],
-      icons: {
-        "fix-checked": "https://img.icons8.com/offices/35/000000/ok.png",
-        "fix-unchecked":
-          "https://img.icons8.com/material-two-tone/35/000000/marker.png",
-        checked: "https://img.icons8.com/color/35/000000/ok--v1.png",
-        unchecked: "https://img.icons8.com/ios-filled/35/000000/marker.png",
-      },
-    };
-  },
-  computed: {
-    isLogin() {
-      return this.$store.state.isLogin;
-    },
-  },
-  methods: {
-    toggleInfoWindow: function(marker, idx) {
-      this.infoWindowPos = marker.position;
-      this.infoContent = marker;
-
-      //check if its the same marker that was selected if yes toggle
-      if (this.currentMidx == idx) {
-        this.infoWinOpen = !this.infoWinOpen;
-      }
-      //if different marker set infowindow to open and reset current marker index
-      else {
-        this.infoWinOpen = true;
-        this.currentMidx = idx;
-      }
-    },
-    checkin(gostation_id) {
-      const config = {
-        headers: {
-          Authorization: `Bearer ${this.$store.state.token}`,
+        markers: [],
+        infoContent: null,
+        infoWindowPos: {
+          lat: 0,
+          lng: 0
         },
-      };
-      this.$http.defaults.headers.common = {
-        Authorization: `Bearer ${this.$store.state.token}`,
-      };
-      this.$http
-        .post("http://192.168.1.102:5000/checkin", {
-          gostation_id: gostation_id,
-        })
-        .then(
-          (res) => {
-            alert("簽到成功");
-          },
-          (err) => {
-            this.$store.commit("SET_AUTH", {
-              token: "",
-              isLogin: false,
-            });
-            this.$router.replace("/login");
+        infoWinOpen: false,
+        currentMidx: null,
+        //optional: offset infowindow so it visually sits nicely on top of our marker
+        infoOptions: {
+          pixelOffset: {
+            width: 0,
+            height: -35
           }
-        );
-    },
-  },
-  mounted() {
-    if (!("geolocation" in navigator)) {
-      return;
-    }
-    navigator.geolocation.getCurrentPosition((pos) => {
-      this.center = {
-        lat: pos.coords.latitude,
-        lng: pos.coords.longitude,
+        },
+        GoStations: [],
+        // marker_config: [],
+        history: [],
+        check_list: [],
+        icons: {
+          "fix-checked": "https://img.icons8.com/offices/35/000000/ok.png",
+          "fix-unchecked":
+            "https://img.icons8.com/material-two-tone/35/000000/marker.png",
+          checked: "https://img.icons8.com/color/35/000000/ok--v1.png",
+          unchecked: "https://img.icons8.com/ios-filled/35/000000/marker.png"
+        }
       };
-      this.$http.defaults.headers.common = {};
-      this.$http.get("https://webapi.gogoro.com/api/vm/list").then((res) => {
-        this.GoStations = res.data;
+    },
+    computed: {
+      isLogin() {
+        return this.$store.state.isLogin;
+      }
+    },
+    methods: {
+      toggleInfoWindow: function(marker, idx) {
+        this.infoWindowPos = marker.position;
+        this.infoContent = marker;
+
+        //check if its the same marker that was selected if yes toggle
+        if (this.currentMidx == idx) {
+          this.infoWinOpen = !this.infoWinOpen;
+        }
+        // //if different marker set infowindow to open and reset current marker index
+        else {
+          this.infoWinOpen = true;
+          this.currentMidx = idx;
+        }
+      },
+      checkin(infoContent, idx) {
         const config = {
           headers: {
-            Authorization: `Bearer ${this.$store.state.token}`,
-          },
+            Authorization: `Bearer ${this.$store.state.token}`
+          }
         };
         this.$http.defaults.headers.common = {
-          Authorization: `Bearer ${this.$store.state.token}`,
+          Authorization: `Bearer ${this.$store.state.token}`
         };
         this.$http
-          .get("http://192.168.1.102:5000/history")
+          .post("http://192.168.1.102:5000/checkin", {
+            gostation_id: infoContent.id
+          })
           .then(
-            (res) => {
-              return res.data.data;
+            res => {
+              alert("簽到成功");
+              infoContent.checked = true;
+              this.markers.slice(idx, 1, infoContent);
             },
-            (err) => {
-              return [];
+            err => {
+              this.$store.commit("SET_AUTH", {
+                token: "",
+                isLogin: false
+              });
+              this.$router.replace("/login");
             }
-          )
-          .then((res) => {
-            this.history = res;
-            this.history.forEach((record) => {
-              if (!this.check_list.includes(record["gostation_id"])) {
-                this.check_list.push(record["gostation_id"]);
+          );
+      }
+    },
+    mounted() {
+      if (!("geolocation" in navigator)) {
+        return;
+      }
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.center = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        };
+        this.$http.defaults.headers.common = {};
+        this.$http.get("https://webapi.gogoro.com/api/vm/list").then(res => {
+          this.GoStations = res.data;
+          const config = {
+            headers: {
+              Authorization: `Bearer ${this.$store.state.token}`
+            }
+          };
+          this.$http.defaults.headers.common = {
+            Authorization: `Bearer ${this.$store.state.token}`
+          };
+          this.$http
+            .get("http://192.168.1.102:5000/history")
+            .then(
+              res => {
+                return res.data.data;
+              },
+              err => {
+                return [];
               }
+            )
+            .then(res => {
+              this.history = res;
+              this.history.forEach(record => {
+                if (!this.check_list.includes(record["gostation_id"])) {
+                  this.check_list.push(record["gostation_id"]);
+                }
+              });
+              this.GoStations.forEach((location, idx) => {
+                let marker = {
+                  // 設定為該餐廳的座標
+                  name: JSON.parse(location["LocName"])["List"][1]["Value"],
+                  address: JSON.parse(location["Address"])["List"][1]["Value"],
+                  time: location["AvailableTimeByte"],
+                  state: location["State"],
+                  position: {
+                    lat: location["Latitude"],
+                    lng: location["Longitude"]
+                  },
+                  checked: this.check_list.includes(location["Id"]),
+                  id: location["Id"]
+                };
+                this.$set(this.markers, idx, marker);
+              });
+              this.GoStations = [];
+              this.history = [];
+              this.check_list = [];
             });
-            this.GoStations.forEach((location, idx) => {
-              let marker = {
-                // 設定為該餐廳的座標
-                name: JSON.parse(location["LocName"])["List"][1]["Value"],
-                address: JSON.parse(location["Address"])["List"][1]["Value"],
-                time: location["AvailableTimeByte"],
-                state: location["State"],
-                position: {
-                  lat: location["Latitude"],
-                  lng: location["Longitude"],
-                },
-                checked: this.check_list.includes(location["Id"]),
-                id: location["Id"],
-              };
-              this.$set(this.markers, idx, marker);
-            });
-          });
+        });
       });
-    });
-  },
-};
+    }
+  };
 </script>
 
 <style scoped>
-#map {
-  margin: 0px;
-  padding: 0px;
-  width: 100%;
-  height: calc(100vh - 56px);
-}
-.title {
-  font-weight: 500;
-  font-size: 20px;
-}
+  #map {
+    margin: 0px;
+    padding: 0px;
+    width: 100%;
+    height: calc(100vh - 56px);
+  }
+  .title {
+    font-weight: 500;
+    font-size: 20px;
+  }
 </style>
